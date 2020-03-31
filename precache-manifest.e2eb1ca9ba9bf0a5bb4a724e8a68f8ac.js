31f59bbf4ed3ede236a8b00051ba2baec16f4150
alex <alextotalk@gmail.com>
2020-03-31 16:51:11 +0300

    Updates

---
+++ precache-manifest.e2eb1ca9ba9bf0a5bb4a724e8a68f8ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bcc009b205dafcc8eaf04eec00c6f6a",
+    "url": "/verstka/index.html"
+  },
+  {
+    "revision": "42f2c21d8a5c38911023",
+    "url": "/verstka/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c79bc9666fc53e5dbc79",
+    "url": "/verstka/static/js/2.f6ebad12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/verstka/static/js/2.f6ebad12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42f2c21d8a5c38911023",
+    "url": "/verstka/static/js/main.84b94f78.chunk.js"
+  },
+  {
+    "revision": "1685b2830c7829de162f",
+    "url": "/verstka/static/js/runtime-main.89524ee8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/verstka/static/media/logo.5d5d9eef.svg"
+  }
+]);
