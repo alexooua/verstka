0da64fd3068daaf7372d8ba896f1a128491f2677
alex <alextotalk@gmail.com>
2020-04-17 14:02:07 +0300

    first

--- src/App.js
+++ src/App.js
@@ -16,108 +16,80 @@ function App() {
               </nav>
           </header>
           <main>
-
-              <section className="blockFirst">Блок1
-                  <div className="blockFirstHello">
-                      <h1>Hi my is Ivan Ivanich</h1>
-                      <img src="" alt="Photo"/>
-                  </div>
+              <section className="blockFirst">
+                  <h1>Hi my is Ivan Ivanich</h1>
+                  <img src="" alt="Photo"/>
               </section>
-
-              <section className="blockTwo">Блок2
-                  <div className="skills">
-                      <div className="skillsHead">
-                          <h3>My skills</h3>
-                      </div>
-
-                      <div className='skillsItem'>
-                          <div className="skill">
-                              <div><img src="" alt="im"/>
-                                  <h3>React</h3>
-                              </div>
-                              <p>About skills</p>
-                          </div>
-                          <div className="skill">
-                              <div><img src="" alt="im"/>
-                                  <h3>React</h3>
-                              </div>
-                              <p>About skills</p>
-                          </div>
-                          <div className="skill">
-                              <div><img src="" alt="im"/>
-                                  <h3>React</h3>
-                              </div>
-                              <p>About skills</p>
-                          </div>
-                      </div>
+              <section className="blockTwo">
+                  <div className="skillsHead">
+                      <h3>My skills</h3>
                   </div>
-
-              </section >
-              <section className="blockThree">Блок3
-                  <div className="works">
-                      <div>
-                          <h3>Мои работы</h3>
+                  <div className='skillsItem'>
+                      <div className="skill">
+                          <img src="" alt="im"/>
+                          <h3>React</h3>
+                          <p>About skills</p>
                       </div>
-                      <div className="worksItems" >
-                          <div className="work">
-                              <div className="workImg">Картинка</div>
-                              <button>Show</button>
-                              <h4>Name project</h4>
-                              <p>Some little description</p>
-
-                          </div>
-                          <div className="work">
-                              <div className="workImg">Картинка</div>
-                              <button>Show</button>
-                              <h4>Name project</h4>
-                              <p>Some little description</p>
-                          </div>
+                      <div className="skill">
+                          <img src="" alt="im"/>
+                          <h3>React</h3>
+                          <p>About skills</p>
+                      </div>
+                      <div className="skill">
+                          <img src="" alt="im"/>
+                          <h3>React</h3>
+                          <p>About skills</p>
                       </div>
                   </div>
               </section>
-              <section>Блок4
-                  <div>
-                      <h3>I want to work far from you!!!!</h3>
+              <section className="blockThree">
+                  <div className="worksHead">
+                      <h3>Мои работы</h3>
+                  </div>
+                  <div className="worksItems">
+                      <div className="work">
+                          <div className="workImg">Картинка</div>
+                          <button>Show</button>
+                          <h4>Name project</h4>
+                          <p>Some little description</p>
 
-                      <button>HIRE ME
-                      </button>
-                  </div></section>
-              <section>Блок5
-                  <div>
+                      </div>
+                      <div className="work">
+                          <div className="workImg">Картинка</div>
+                          <button>Show</button>
+                          <h4>Name project</h4>
+                          <p>Some little description</p>
+                      </div>
+                  </div>
+
+              </section>
+              <section className="hire">
+                  <h3>I want to work far from you!!!!</h3>
+                  <button>HIRE ME
+                  </button>
+              </section>
+              <section>
+                  <form className="message">
                       <h3>Contact</h3>
-                      <form>
-                          <input type="text" value="<input>"/>
-                          <input type="text" value="<input>"/>
-                          <textarea value="</textarea>">
-
-                          </textarea>
-
-                      </form>
-                      <button>Send
-                      </button>
-                  </div>
+                      <input className="messageName" type="text" placeholder="Name"/>
+                      <input className="messageEmail" type="email" placeholder="E-mail"/>
+                      <textarea placeholder="Your message"> </textarea>
+                      <button>Send</button>
+                  </form>
               </section>
-
           </main>
-          <footer>Footer
-              <div>
-                  <h3>
-                      Ivan Ivanov
-                  </h3>
-                  <div className="">
-                      <div></div>
-                      <div></div>
-                      <div></div>
-                      <div></div>
-                      <div></div>
-                  </div>
-                  <h5>
-                      2019 All is ok
-                  </h5>
+          <footer>
+              <h3>Ivan Ivanov</h3>
+              <div className="follow">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
               </div>
-
+              <h5>2019 All is ok</h5>
           </footer>
-
+          <div className="container"></div>
       </div>
   );
 }
